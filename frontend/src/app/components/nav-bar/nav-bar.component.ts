d25885fd585f8e9667baa88b3f16f8743d696fc7
Strahinja <Strahinja@LAPTOP-13HLLGCJ>
2023-06-28 14:26:53 +0200

    nav-bar

--- frontend/src/app/components/nav-bar/nav-bar.component.ts
+++ frontend/src/app/components/nav-bar/nav-bar.component.ts
@@ -1,8 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/service/auth-service';
 
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.css'],
 })
-export class NavBarComponent {}
+export class NavBarComponent implements OnInit {
+  constructor(public router: Router, private auth: AuthService) {}
+
+  ngOnInit(): void {}
+
+  loginClick() {
+    this.router.navigateByUrl('/login');
+  }
+
+  registrationClick() {
+    this.router.navigateByUrl('/registration');
+  }
+
+  loggedIn(): Boolean {
+    return this.auth.tokenIsPresent();
+  }
+
+  logout() {
+    return this.auth.logout();
+  }
+}
